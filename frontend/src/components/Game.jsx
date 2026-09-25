a02c88d4de92bad99c25c9d2689016cc3c46f03f
PoisonMunna <123razz321@gmail.com>
2026-09-25 21:59:13 +0530

    update

--- frontend/src/components/Game.jsx
+++ frontend/src/components/Game.jsx
@@ -199,7 +199,7 @@ export default function Game({
                       {gameState.lastRoundResult.correctGuess ? "Sipahi Guessed Correctly! 🛡️" : "Sipahi Guessed Wrong! ☠️"}
                     </h3>
                     <p className="text-slate-300 mt-2">
-                      Sipahi chose {gameState.lastRoundResult.guessedPlayerName}. {gameState.lastRoundResult.correctGuess ? "CHOR WIN !" : "CHOR LOSS !"}
+                      Sipahi chose {gameState.lastRoundResult.guessedPlayerName}. {gameState.lastRoundResult.correctGuess ? "CHOR ESCAPED !" : "CHOR ARRESTED !"}
                     </p>
                   </div>
                 </>
